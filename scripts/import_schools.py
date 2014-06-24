986fe55bd5f43ab604c76a6e12c7f7e34775a448
haiqiang <haiqiangz1991@gmail.com>
2014-06-24 13:10:00 -0400

    script and data for school

---
+++ scripts/import_schools.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+import sys
+from pymongo import MongoClient
+
+
+#connects to the MongoDB
+client = MongoClient()
+db = client.schools
+collection = db.school_info
+
+
+if len(sys.argv) == 1:
+    print('Usage: %s schools.csv' % sys.argv[0])
+    sys.exit(1)
+
+
+headers = ["SCHOOL NAME"]
+
+headers2 = [
+    'Location Code',
+    'Location Name',
+    'Location Category Description',
+    'Primary Address',
+    'City',
+    'State Code',
+    'Zip',
+    'Principal Phone Number'
+]
+
+
+offset = {}
+school_info = {}
+
+
+with open(sys.argv[1]) as f:
+
+
+    count  = 0;
+    for index, line in enumerate(f):
+        fields = line.split('\t')
+        
+        if index == 0:
+
+            for field in headers:
+               offset[field] = fields.index(field)
+
+        else:
+            count +=1
+            school_name = fields[offset['SCHOOL NAME']]
+            school_name = school_name.lower()
+            school_info[school_name] = True
+
+offset = {}
+
+with open(sys.argv[2]) as e:
+    count = 0
+    for index, line in enumerate(e):
+
+        fields = line.split('\t')
+
+        if index == 0:
+
+            for field in headers2:
+                offset[field] = fields.index(field)
+
+        else:
+            school_name = fields[offset['Location Name']]
+            school_name = school_name.lower()
+            school_address = fields[offset['Primary Address']]
+            phone_number = fields[offset['Principal Phone Number']]
+
+            if school_name in school_info:
+
+                post = {"School_Name" : school_name, "School_Address" : school_address, "Principal_Phone_Number" : phone_number}
+                
+                collection.insert(post)
+
+                    #count +=1
+                school_info[school_name] = school_address
+                    #print '%s        %s' %(school_name, school_address)
+
+'''                
+for i in school_info:
+
+    if school_info[i] == True:
+
+        print i, school_info[i]
+'''
